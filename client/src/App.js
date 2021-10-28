1ac9b27c835c30132f47eb784dd219cfde76fbeb
Chris Lee <christopher.lee222@yahoo.com>
2021-10-27 19:17:55 -0500

    CRUD functional

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+import AddEmployee from "./components/AddEmployee";
+import EditEmployee from "./components/EditEmployee";
+import { GlobalProvider } from "./context/GlobalState";
+import Home from "./pages/Home";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      CRUD
-    </div>
+    <GlobalProvider>
+      <div className="App">
+        <Switch>
+          <Route path="/edit/:id" component={EditEmployee} exact />
+          <Route path="/add" component={AddEmployee} exact />
+          <Route path="/" component={Home} exact />
+        </Switch>
+      </div>
+    </GlobalProvider>
   );
-}
+};
 
 export default App;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Heading from "../components/Heading";
+import EmployeeList from "../components/EmployeeList";
+
+const Home = () => {
+    
+  return (
+    <div className="container mx-auto">
+      <h3 className="text-center text-3xl mt-20 text-base leading-8 text-black font-bold tracking-wide uppercase">
+        CRUD with React Context API and Hooks
+      </h3>
+      <Heading />
+      <EmployeeList />
+    </div>
+  );
+};
+
+export default Home;
